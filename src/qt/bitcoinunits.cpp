efd82940d0a35794698f3947368f3fcefb86ae39
mojo47 <mojo.ruppert@gmail.com>
2021-05-31 20:45:03 +0200

    photons/sprinkles Photons/Sprinkles Lites/Poo

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -42,7 +42,7 @@ QString BitcoinUnits::name(int unit)
     {
     case BTC: return QString("SHT");
     case mBTC: return QString("poo");
-    case uBTC: return QString("photons");
+    case uBTC: return QString("sprinnkles");
     default: return QString("???");
     }
 }
@@ -52,8 +52,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("Shits");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case mBTC: return QString("Poo (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("Sprinnkles (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
